1670948066a7acec82dc802d213df97ac1474cef
devEyosiyas <mreyosivip@gmail.com>
2020-09-08 07:53:46 +0300

    Project commit.

--- app/src/main/java/dev/gdgaddis/codenightsmsblocker/service/BlockerService.java
+++ app/src/main/java/dev/gdgaddis/codenightsmsblocker/service/BlockerService.java
@@ -1,4 +1,16 @@
 package dev.gdgaddis.codenightsmsblocker.service;
 
-public class BlockerService {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.util.Log;
+
+public class BlockerService extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
 }
